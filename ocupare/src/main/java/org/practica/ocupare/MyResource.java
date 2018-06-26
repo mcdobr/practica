787235dbe80c93e64e47fc04777765bef0cd01bb
StoianSabina <sabina.stoian@gmail.com>
2018-06-26 10:00:03 +0300

    Am facut sa mearga cu Hibernate

--- ocupare/src/main/java/org/practica/ocupare/MyResource.java
+++ ocupare/src/main/java/org/practica/ocupare/MyResource.java
@@ -5,6 +5,10 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Root resource (exposed at "myresource" path)
  */
@@ -22,15 +26,29 @@ public class MyResource {
     public String getIt() {
         
     	
+        Eveniment eveniment = new Eveniment();
+    	eveniment.setId(1);
+    	eveniment.setNume("First event");
+    	SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+    	Session session = sessionFactory.openSession();
+    	session.beginTransaction();
+    	session.save(eveniment);
     	
+    	eveniment = new Eveniment();
+    	eveniment.setId(2);
+    	eveniment.setNume("Second event");
+    	session.save(eveniment);
     	
+    	session.getTransaction().commit();
+    	session.close();
     	
-    	
-    	return "Got it!";
+        return "Got it!";
         
         
         
         
         
     }
+    
+
 }
